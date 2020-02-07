c341e7c5073623fe52019507c83c399b07999d6f
Jack <jack.huey@umassmed.edu>
2020-02-06 20:20:44 -0500

    Initial implementation of the Program Writer
    This is squashed from several WIP commits. The first being authored
    Jack Huey, and the following all authored by David Ross and Super
    Tuple.
    
    The majority of the structure here was decided and written by Super
    Tuple and David Ross.
    
    Co-authored-by: David Ross <daboross@daboross.net>
    Co-authored-by: Super Tuple <supertuple@gmail.com>

--- chalk-ir/src/lib.rs
+++ chalk-ir/src/lib.rs
@@ -264,6 +264,7 @@ impl UniverseMap {
 #[derive(Copy, Clone, PartialEq, Eq, PartialOrd, Ord, Hash)]
 pub struct AdtId<I: Interner>(pub I::InternedAdtId);
 
+
 /// The id of a trait definition; could be used to load the trait datum by
 /// invoking the [`trait_datum`] method.
 ///
@@ -404,7 +405,7 @@ pub enum TyData<I: Interner> {
     /// an empty list).
     Apply(ApplicationTy<I>),
 
-    /// instantiated form a universally quantified type, e.g., from
+    /// instantiated from a universally quantified type, e.g., from
     /// `forall<T> { .. }`. Stands in as a representative of "some
     /// unknown type".
     Placeholder(PlaceholderIndex),
@@ -1161,6 +1162,8 @@ pub struct OpaqueTy<I: Interner> {
     pub substitution: Substitution<I>,
 }
 
+/// Represents a trait implementation for a struct. The first argument is the
+/// struct, whereas any arguments after that are trait parameters.
 #[derive(Clone, PartialEq, Eq, Hash, Fold, Visit, HasInterner, Zip)]
 pub struct TraitRef<I: Interner> {
     pub trait_id: TraitId<I>,
